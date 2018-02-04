92102a3921bba558b7127a31f1e7c2a334915dc9
shibamoto <shibamoto@tgl.co.jp>
2018-02-04 16:11:06 +0900

    delete item

--- index.ios.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {
-  AppRegistry,
-} from 'react-native';
-
-import App from './src/native/components/App.react'
-
-AppRegistry.registerComponent('CrossDeviceTodoExample', () => App);
