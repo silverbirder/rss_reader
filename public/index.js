dd1148e688c27e20cda7094e441964b3d26f7517
shibamoto <shibamoto@tgl.co.jp>
2018-02-13 00:12:49 +0900

    delete index.electron.js

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ let mainWindow
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL(`file://${__dirname}/public/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
   // mainWindow.webContents.openDevTools() // For development
   mainWindow.on('closed', function() {
     mainWindow = null
